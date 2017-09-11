98265685f8710175b332cb404b665f1cf2ea6ec0
Yuze Zhou <zhouyuze@hashdata.cn>
2017-09-11 16:19:35 +0800

    Lab1 part3

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (Map
@@ -32,5 +35,27 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+	var wg sync.WaitGroup
+	for i:=0; i< ntasks; i++ {
+		wg.Add(1)
+		go func(taskNum int, nios int, phase jobPhase) {
+			for {
+				worker := <- registerChan
+				var args DoTaskArgs
+				args.JobName = jobName
+				args.File = mapFiles[taskNum]
+				args.Phase = phase
+				args.TaskNumber = taskNum
+				args.NumOtherPhase = nios
+				ok := call(worker, "Worker.DoTask", args, new(struct{}))
+				if ok {
+					wg.Done()
+					registerChan <- worker
+					break
+				}
+			}
+		}(i, n_other, phase)
+	}
+	wg.Wait()
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
